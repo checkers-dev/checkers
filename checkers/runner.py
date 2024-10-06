e2a8058a3a6c684e08f8d4a37b2d03f07a640f90
Tom Waterman <tjwaterman99@gmail.com>
2024-10-06 18:53:13 +0000

    Cache results on the Runner

--- checkers/runner.py
+++ checkers/runner.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, List
 from .contracts import CheckResult
 from .collectors import CheckCollector, ModelCollector
 from .printer import Printer
@@ -11,8 +11,11 @@ class Runner:
         self.model_collector = model_collector
         self.printer = printer
         self.config = config
+        self.results: List[CheckResult] = list()
 
     def run(self) -> Iterable[CheckResult]:
         for model in self.model_collector.collect():
             for check in self.check_collector.collect():
-                yield check.run(model)
+                res = check.run(model)
+                self.results.append(res)
+                yield res
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -5,4 +5,5 @@ from checkers.contracts import CheckResult
 def test_runner_runs(runner: Runner):
     results = list(runner.run())
     assert len(results) > 0
+    assert len(runner.results) > 0
     assert isinstance(results[0], CheckResult)
