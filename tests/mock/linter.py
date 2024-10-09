0f896627c32cf82de1fe9f697b137409e5d35850
Tom Waterman <tjwaterman99@gmail.com>
2024-10-09 15:00:50 +0000

    Remove that

--- tests/mock/linter.py
+++ tests/mock/linter.py
@@ -3,7 +3,3 @@ from checkers.contracts import Model
 
 def check_custom_check(model: Model):
     pass
-
-
-def check_model_has_owner(model: Model):
-    assert 'owner' in model.meta, "Model must specify an `owner` field in its `meta` block"
