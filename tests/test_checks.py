8639f74cfa80b760f50e91bade668a9ceb6fa2e8
Tom Waterman <tjwaterman99@gmail.com>
2024-10-10 13:13:23 +0200

    Add test for check_model_has_primary_key (#15)

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -1,6 +1,7 @@
+from unittest.mock import MagicMock
 from checkers import checks, Model
 from checkers.core import Checker
-from checkers.contracts import CheckResultStatus
+from checkers.contracts import CheckResultStatus, Column, Test, Manifest
 from checkers.config import Config
 
 
@@ -19,5 +20,13 @@ def test_check_model_has_description(model: Model, config: Config):
     assert checker.run(model).status == CheckResultStatus.passing, res.message
 
 
-def test_check_model_has_priamry_key_test(model: Model, config: Config):
-    pass
+def test_check_model_has_primary_key_test(config: Config, manifest: Manifest):
+    model = manifest.get_model_by_name("my_first_dbt_model")
+    assert model is not None
+    checker = Checker(check=checks.check_model_has_primary_key_test, config=config)
+    assert checker.run(model).status == CheckResultStatus.passing
+
+    model = manifest.get_model_by_name("my_second_dbt_model")
+    assert model is not None
+    checker = Checker(check=checks.check_model_has_primary_key_test, config=config)
+    assert checker.run(model).status == CheckResultStatus.failure
